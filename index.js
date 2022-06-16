4fb7be204a4b7728551860a557c723a617f58fec
root <root@HomeLPT-001.localdomain>
2022-06-17 01:52:11 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Add your code here
+function submitData(name, email) {
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        })
+    })
+        .then(res => res.json())
+        .then(user => document.querySelector('body').append(user.id))
+        .catch(errorObj => document.querySelector('body').append(errorObj.message))
+} 
+
